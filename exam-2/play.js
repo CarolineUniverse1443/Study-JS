ef7e05b578352621395edbdc1c19419c7aa1352c
CarolineUniverse1443 <popovakaro03@gmail.com>
2023-07-28 19:12:58 +0300

    code optimization && start do second game

--- exam-2/play.js
+++ exam-2/play.js
@@ -1,39 +1,61 @@
-alert('Привет, добро пожаловать в "Игромат" 1.0');
-let game = '';
+init();
 
-while (game != '4')
+function init()
 {
-	game = prompt( `
+	alert('Привет, добро пожаловать в "Игромат" 1.0');
+	let game = chooseGame();
+}
+
+function chooseGame()
+{
+	let game = '';
+
+	while (game != '4')
+	{
+		game = prompt( `
 Выбери игру
 1 - угадалка;
 2 - считалка;
 3 - кликалка;
 Для выхода введи "выход"
-`, '1');
+`, '2');
 
-	switch (game)
-	{
-		case '1':
-		case 'угадалка':
-			alert('Запускается игра...');
-			guessGame();
-			break;
-		case '2':
-		case 'считалка':
-			alert('Запускается игра...');
-			break;
-		case '3':
-		case 'кликалка':
-			alert('Запускается игра...');
-			break;
-		case '4':
-			alert('Пока, пока :3');
-			break;
-		default:
-			alert('Неправильный ввод :с');
-			break;
+		switch (game)
+		{
+			case '1':
+			case 'угадалка':
+			case '2':
+			case 'считалка':
+			case '3':
+			case 'кликалка':
+				startGame(game);
+				break;
+			case '4':
+				alert('Пока, пока :3');
+				break;
+			default:
+				alert('Неправильный ввод :с');
+				break;
+		}
 	}
 }
+
+function startGame(game)
+{
+	alert('Запускается игра...');
+	if(game == '1' || game == 'угадалка')
+		guessGame();
+	if(game == '2' || game == 'считалка')
+		solveGame();
+	if(game == '3' || game == 'кликалка')
+		clickGame();
+}
+
+function random(range)
+{
+	return Math.floor(Math.random() * range);
+}
+
 
 function guessGame()
 {
@@ -44,7 +66,7 @@ function guessGame()
     После каждого твоего ввода я буду говорить больше твое число или меньше заданного.
     Начнем?
 `);
-	let randomNumber = Math.floor(Math.random() * 100);
+	let randomNumber = random(100);
 	alert(randomNumber);
 	let guessNumber = '';
 	let counter = 0;
@@ -76,3 +98,42 @@ function guessGame()
 
 	alert(`Тебе удалось угадать за ${counter} попыток`);
 };
+
+function solveGame()
+{
+	let num1 = 0;
+	let num2 = 0;
+	let count = 0;
+	let guess = '';
+	alert('Игра "считалка"');
+	alert(`
+    Я случайным образом буду давать задание по арифметике.
+    Твоя задача правильно решить 5 примеров.
+    Начнем?
+`);
+	for (let i = 0; i < 5; i++)
+	{
+		num1 = random(20);
+		num2 = random(20);
+
+		guess = prompt(`${num1} + ${num2}`);
+
+		if(+guess == num1+num2)
+		{
+			count++;
+		}
+	}
+
+	alert(`Правильных ответов: ${count} из 5`);
+}
+
+function clickGame()
+{
+	alert('Игра "кликалка"');
+	alert(`
+    Я случайным образом буду показывать 10 системных окон.
+    Твоя задача как можно скорее прокликать их все.
+    В окне confirm нужно нажимать "Отмена".
+    Начнем?
+`);
+}
